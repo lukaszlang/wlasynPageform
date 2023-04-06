42371be089a07ee9739444e8a0c357071e6c3e42
Lukasz L <lukasz.lang@sigma.info.pl>
2023-04-06 11:17:26 +0200

    aaa

---
+++ wlasynPageform/wlasynPageform/MojWorker.cs
@@ -0,0 +1,23 @@
+﻿using Soneta.Business;
+using Soneta.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[assembly: Worker(typeof(wlasynPageform.MojWorker), typeof(Soneta.Towary.Towar))]
+
+namespace wlasynPageform
+{
+    public class MojWorker
+    {
+
+
+        [Action("Uruchom worker", Target = ActionTarget.ToolbarWithText, Mode = ActionMode.SingleSession, Icon = ActionIcon.Book, Priority = 110)]
+        public object worker()
+        {
+            return new MojaKlasa();
+        }
+
+    }
+}
---
+++ wlasynPageform/wlasynPageform/MojaKlasa.cs
@@ -0,0 +1,22 @@
+﻿using Soneta.Business;
+using Soneta.Business.App;
+
+using wlasynPageform;
+
+
+// Sposób w jaki należy zarejestrować extender, który później zostanie użyty w interfejsie.
+[assembly: Worker(typeof(MojaKlasa))]
+
+
+
+namespace wlasynPageform
+{
+    public class MojaKlasa
+    {
+        [Context]
+        public Session Session { get; set; }
+
+        [Context]
+        public Login Login { get; set; }
+    }
+}
